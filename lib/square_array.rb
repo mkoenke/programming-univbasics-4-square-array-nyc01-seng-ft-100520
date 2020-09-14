bab558f08d5a3761ae0f5d14cfeadaa59213320a
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-14 19:45:08 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
-  # your code here
+  counter = 0 
+  new_array  = []
+  while counter < array.length do
+    new_array.push(array[counter]**2)
+    counter += 1
+  end
+  new_array
 end
+
+#def square_array(array)
+ # new_array = []
+ # array.length.times do |index|
+  #  new_array.push(array[index]**2)
+ # end 
+#end
